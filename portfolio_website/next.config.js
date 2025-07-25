abd52310c84c75b1d7c167f2868afe20f1fff823
Gihan Sanjeewa <123794603+GihanSanjeewa@users.noreply.github.com>
2025-07-25 22:18:08 +0530

    Update next.config.js

--- portfolio_website/next.config.js
+++ portfolio_website/next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const isProd = process.env.NODE_ENV === 'production';
 
-module.exports = nextConfig
+const nextConfig = {
+  output: 'export', // Enables static export
+  basePath: isProd ? '/My-Portfolio-website' : '', // Matches your repo name
+  assetPrefix: isProd ? '/My-Portfolio-website/' : '', // Ensures assets load correctly
+  images: {
+    unoptimized: true, // Disables image optimization for static export
+  },
+};
+
+module.exports = nextConfig;
